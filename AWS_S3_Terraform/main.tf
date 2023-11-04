3e1c73c60f34103a42c46cc3dd85dc3378d462d2
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-11-04 13:31:23 +0530

    Create main.tf

---
+++ AWS_S3_Terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+}
+/* terraform {
+  required_version = "= 0.12.24"
+}
+# variable "access_key" {}
+
+# variable "secret_key" {}
+
